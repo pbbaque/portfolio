516a29134163e1bf9665c6aeeb635f2f27ba679e
pbbaque <pbbaque@gmail.com>
2026-06-02 13:44:02 +0200

    update copy

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="text-2xl font-bold">
-            <span className="gradient-text">PBBaque</span>
+            <span className="gradient-text">pbbaque</span>
           </div>
           <div className="hidden md:flex items-center space-x-8">
             <Link
